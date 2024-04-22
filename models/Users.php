c367705c80a306ef9e019c19df4f49e6f4d8277a
Rutikap07 <rutikapatil270@gmail.com>
2024-04-23 00:53:28 +0530

    Done

---
+++ models/Users.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+class Users extends ActiveRecord implements IdentityInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'fullname', 'password', 'email', 'level'], 'required'],
+            [['level', 'is_deleted'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['username', 'fullname', 'email'], 'string', 'max' => 100],
+            ['password', 'string', 'min' => 8], // Minimum length of 8 characters
+            ['password', 'validatePasswordComplexity'], // Custom validation for password complexity
+        ];
+    }
+
+    public function validatePasswordComplexity($attribute, $params)
+{
+    if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/', $this->$attribute)) {
+        $this->addError($attribute, 'Password must contain at least one uppercase letter, one lowercase letter, and one digit.');
+    }
+}
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'User ID',
+            'username' => 'Username',
+            'fullname' => 'Fullname',
+            'password' => 'Password',
+            'email' => 'Email',
+            'level' => 'Level',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'is_deleted' => 'Is Deleted',
+        ];
+    }
+
+    /**
+     * Finds an identity by the given ID.
+     * @param string|int $id the ID to be looked for
+     * @return IdentityInterface|null the identity object that matches the given ID.
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    /**
+     * Finds an identity by the given token.
+     * @param string $token the token to be looked for
+     * @param mixed $type the type of the token. The value of this parameter depends on the implementation.
+     * For example, it can be an access token or a refresh token.
+     * @return IdentityInterface|null the identity object that matches the given token.
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        // Implement this method according to your application's logic
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return null;
+    }
+
+    /**
+     * Finds user by username.
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::findOne(['email' => $username]);
+    }
+
+    /**
+     * Validates password.
+     * @param string $password
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+}
